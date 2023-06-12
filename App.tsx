eb32ad467939a2b46933f80e5a005964688a21db
Mr WANG <2362999724@qq.com>
2023-06-12 10:53:36 +0800

    add top tab

--- App.tsx
+++ App.tsx
@@ -1,19 +1,81 @@
+/* eslint-disable react/no-unstable-nested-components */
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import DawerScreen from './src/pages/Drawer';
-import HomeScreen from './src/pages/Home';
+import HomeScreen from '@/pages/Home';
+import MineScreen from '@/pages/Mine/Mine';
+import LoanScreen from '@/pages/Loan';
+
+import {Image, ImageSourcePropType} from 'react-native';
 
 const Tab = createBottomTabNavigator();
+
+interface ITabBarIconProp {
+  focused: boolean;
+  color: string;
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="DawerScreen" component={DawerScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({color, focused}: ITabBarIconProp) => (
+              <FooterIcon
+                focused={focused}
+                activedIcon={require('@/assets/images/sy-act.png')}
+                inActivedIcon={require('@/assets/images/sy.png')}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Loan"
+          component={LoanScreen}
+          options={{
+            headerShown: true,
+            tabBarIcon: ({color, focused}) => (
+              <FooterIcon
+                focused={focused}
+                activedIcon={require('@/assets/images/dk-act.png')}
+                inActivedIcon={require('@/assets/images/dk.png')}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Mine"
+          component={MineScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <FooterIcon
+                focused={focused}
+                activedIcon={require('@/assets/images/sy-act.png')}
+                inActivedIcon={require('@/assets/images/sy.png')}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
+}
+
+interface IFooterIconProp {
+  focused: boolean;
+  activedIcon: ImageSourcePropType;
+  inActivedIcon: ImageSourcePropType;
+}
+//IndexIcon
+function FooterIcon({focused, activedIcon, inActivedIcon}: IFooterIconProp) {
+  const iconStyle = {width: 18, height: 18};
+  if (focused) {
+    return <Image source={activedIcon} style={iconStyle} />;
+  } else {
+    return <Image source={inActivedIcon} style={iconStyle} />;
+  }
 }
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@/utils': './src/utils',
+          '@/styles': './src/styles',
+          '@/actions': './src/actions',
+          '@/components': './src/components',
+          '@/services': './src/services',
+        },
+      },
+    ],
+  ],
 };
